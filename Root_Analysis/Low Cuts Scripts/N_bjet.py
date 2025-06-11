bbef6876ad34fffd6adeb211c0f9f44e96d16272
Gabriel V. Vian <gabriel.vian@unesp.br>
2025-06-10 21:31:04 -0300

    Add files via upload

---
+++ Root_Analysis/Low Cuts Scripts/N_bjet.py
@@ -0,0 +1,215 @@
+#!/usr/bin/env python
+############################################################
+#               Code for generating multiple jets plots
+#               Gabriel V. Vian
+#               MsC Student from Institute of Theoretical Physics -- IFT UNESP
+############################################################
+#	ATTENTION: CHANGE ACCORDING TO YOUR INSTALLATION
+#
+# this is needed if running outside Delphes directory
+
+DelphesDirectory = "/home/gabriel/Desktop/MG5_aMC_v2_9_17/Delphes"
+#/Desktop/MG5_aMC_v2_9_17$
+
+############################################################
+
+############################################################
+#	INITIALIZATION BLOCK
+#
+import sys
+import ROOT
+import os
+import math
+import subprocess #to allow me to open the pictures and do not interrupt the code 
+
+
+# Check for user input
+if len(sys.argv) < 1:
+    print("Please input the correct number of .ROOT files!")
+    sys.exit(1)
+
+# Saving the absolute path to the ".root" input files
+inputFiles = sys.argv[1:]
+inputFiles = [os.path.abspath(file) for file in inputFiles]
+
+############################################################
+
+############################################################
+#	DELPHES/ROOT LIBRARIES BLOCK
+#
+# Save current directory
+current_dir = os.getcwd()
+
+# Change to Delphes directory
+os.chdir(DelphesDirectory)
+new_dir = os.getcwd()
+
+# Loading Delphes libraries
+ROOT.gSystem.Load("libDelphes")
+ROOT.gInterpreter.Declare('#include "classes/DelphesClasses.h"')
+ROOT.gInterpreter.Declare('#include "external/ExRootAnalysis/ExRootTreeReader.h"')
+
+############################################################
+
+############################################################
+#	Criando diretórios importantes 
+############################################################
+
+# Diretório do script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+output_dir = os.path.join(script_dir, "N_jets")
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+output_file_path = os.path.join(output_dir, "processing_log.txt")
+
+# Excluir o arquivo de log antigo, se existir
+if os.path.exists(output_file_path):
+    os.remove(output_file_path)
+##############################################################
+#Definindo pesos, pro hisgrama distribuir de acordo com a seção de choque:
+##############################################################
+#PESO = (SEÇÃO DE CHOQUE * LUMINOSIDADE)/(NUMERO TOTAL DE EVENTOS GERADOS (10000))
+
+##############################################################
+
+P_1= 7559.214707 #tt
+P_2= 13.19608992 #ttbb
+P_3= 2.690111995 #ttbbbb
+P_4= 2.683539397 #tth
+P_5= 1.155650301 #ttz
+P_6= 0.048070682 #tttt
+P_7= 0.002389038 #signal
+P_8= 0.001228458 #ttzh
+P_9= 0.000411384 #ttzz
+
+lista_de_pesos = [P_1, P_2, P_3, P_4, P_5, P_6, P_8, P_9,  P_7]
+############################################
+#Bloco de leitura de cada arquivo .root
+############################################################
+with open(output_file_path, "a") as output_file:
+#f"  {os.path.basename(inputFiles[i]).replace('100.root', '')}", "f100"
+    for i, inputFile in enumerate(inputFiles):
+        print(f"Reading file {inputFile}")
+        hist_jet_number = ROOT.TH1F("hist_jet_number", f"Number of b- Jets ({os.path.basename(inputFiles[i]).replace('100.root', '')})", 40, 1, 10)
+
+        # Obter o peso correspondente ao arquivo atual
+        peso_atual = 1
+        # Criando cadeia de árvores ROOT
+        chain = ROOT.TChain("Delphes")
+        chain.Add(inputFile)
+
+        # Criando objeto ExRootTreeReader para ler a cadeia de árvores
+        treeReader = ROOT.ExRootTreeReader(chain)
+        numberOfEntries = treeReader.GetEntries()
+
+
+    # A definição dos galhos deve ser feito uma única vez antes de iniciar o loop. Não é necessário (nem recomendado) chamar UseBranch a cada iteração do loop, pois isso pode causar sobrecarga.
+
+            # OBTENDO OS GALHOS
+        branchJet = treeReader.UseBranch("Jet")
+        branchMissingET = treeReader.UseBranch("MissingET")
+        branchHT = treeReader.UseBranch("ScalarHT")
+        branchelec = treeReader.UseBranch("Electron")
+        branchmu = treeReader.UseBranch("Muon")
+
+
+        # Contadores para acompanhar quantos eventos foram selecionados
+        total_events = 0
+        events_passed_MET_HT_nJets = 0
+        events_passed_final_selection = 0
+
+
+        # Loop sobre os eventos
+        for entry in range(numberOfEntries):
+            print(f"Processing event {entry} of file {inputFile}")
+            total_events += 1  # Incrementa o contador de eventos totais
+
+            # Lendo entrada específica
+            treeReader.ReadEntry(entry)
+
+
+            #OBTENDO AS GRANDEZAS DE CADA GALHO
+                
+            missingET = branchMissingET.At(0)  # Assume-se que há apenas um objeto de MET por evento
+            MET_value = missingET.MET        
+            nJets = branchJet.GetEntries()
+            ht_object = branchHT.At(0)  # Assume-se que há apenas um objeto de HT por evento
+            HT_value = ht_object.HT        #jet_PT = branchJet.PT #Não precisa pra esses objetos
+            #jet_ETA = branchJet.ETA
+            #elec_PT = branchelec.PT
+            #mu_PT = branchmu.PT
+            #elec_ETA = branchelec.ETA
+            #mu_ETA = branchmu.ETA
+            #LISTA DE JATOS E LEPTONS SELECIONADOS QUE DEVE SER RESETADA PRA CADA EVENTO NOVO
+            selected_jets = []
+            q_jets = []
+            b_jets = []
+            electrons = []
+            muons = []
+            
+            ###CUT FLOW###
+
+            #APLICANDO CORTES (VERIFICANDO SE O EVENTO SATISFAZ OS CRITÉRIOS)
+            #PRIMEIRA CONDICIONAL
+            if   nJets >= 0 and HT_value > 500 : 
+                events_passed_MET_HT_nJets += 1 #pra eu contar quantos eventos passaram
+                selected_jets = [jet for jet in branchJet if jet.PT >= 40 and abs(jet.Eta) <= 2.5 ]        
+                q_jets = [jet for jet in selected_jets if abs(jet.Flavor)  in [0, 1, 2, 3, 4]  ] 
+                b_jets = [jet for jet in selected_jets if abs(jet.Flavor) == 5]
+                electrons = [electron for electron in branchelec if electron.PT > 20 and abs(electron.Eta) <= 2.5  ] 
+                muons = [muon for muon in branchmu if muon.PT > 20 and abs(muon.Eta) <= 2.5 ] 
+
+            #SEGUNDA CONDICIONAL, DENTRO DA PRIMEIRA, QUE OBSERVA O NUMERO DE OBJETOS
+                if len(q_jets) >= 0 and len(b_jets) >= 0 and len(muons) == 0 and len(electrons) == 0 : 
+                    events_passed_final_selection += 1  # Incrementa o contador para eventos que passam essa seleção final
+
+                # Contar o número de jatos de |sabor| == 5 no evento atual
+                    num_jets_flavor5 = 0
+                    for jetObject in b_jets:
+                        # Verificar se o jato é um jato b e se satisfaz as condições de PT e Eta
+                        if abs(jetObject.Flavor) == 5:
+                            num_jets_flavor5 += 1
+
+                    # Preencher o histograma com o número de jatos de |sabor| == 5 no evento atual
+                    hist_jet_number.Fill(num_jets_flavor5)
+
+                    print(f"Número de jatos b no evento {entry}: {num_jets_flavor5}")
+
+
+            else:
+                    continue  # Se a primeira condicional nao for satisfeita, vá para o próximo event
+        print(f"Total de eventos processados: {total_events}")
+        print(f"Eventos que passaram MET >= 40, HT > 500 e nJets >= 6: {events_passed_MET_HT_nJets}")
+        print(f"Eventos que passaram a seleção final: {events_passed_final_selection}")
+        # Salvar os resultados em um arquivo de texto para o arquivo de entrada atual
+        output_file.write(f"Results for file: {inputFile}\n")
+        output_file.write(f"Total de eventos processados: {total_events}\n")
+        output_file.write(f"Eventos que passaram MET >= 40, HT > 500 e nJets >= 6: {events_passed_MET_HT_nJets}\n")
+        output_file.write(f"Eventos que passaram a seleção final: {events_passed_final_selection}\n")
+        output_file.write("-" * 40 + "\n")
+
+    
+
+  
+
+
+        # Crie um canvas para os histogramas
+        canvas = ROOT.TCanvas("canvas", "Canvas", 1200, 1000)
+        canvas.SetLogy()
+
+        #hist_jet_number.Sumw2(0)
+    #  hist_jet_number.SetLineColor(ROOT.kBlue)
+    #  hist_jet_number.SetLineWidth(2)
+        hist_jet_number.Sumw2(0)
+        hist_jet_number.Draw()
+        legend_PT = ROOT.TLegend(0.75, 0.1, 0.9, 0.3)  
+        legend_PT.SetTextSize(0.03)
+        legend_PT.SetTextFont(42)
+        legend_PT.AddEntry(hist_jet_number, f"  {os.path.basename(inputFiles[i]).replace('100.root', '')}", "f100")
+
+        image_path = os.path.join(output_dir, f"bjet_number_perevent_{os.path.basename(inputFile).replace('.root', '.png')}")
+        canvas.SaveAs(image_path)
+
+
+        # Abra os histogramas quando prontos
+    #  subprocess.Popen(["xdg-open", image_path])
